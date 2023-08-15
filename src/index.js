0cbaa4e337ea455c965bba1d4151c9f19c1ce154
felps <felipetec.sso@gmail.com>
2023-08-14 21:57:53 -0300

    revison clean code

--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import './assets.js/styles/reset.css'; // Corrigindo o caminho
+import './assets.js/styles/reset.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
+// Obtendo o elemento root do DOM
 const root = document.getElementById('root');
+
+// Criando uma raiz (root) concorrente
 const appRoot = ReactDOM.createRoot(root);
 
+// Renderizando o componente App dentro do elemento root em modo estrito
 appRoot.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
