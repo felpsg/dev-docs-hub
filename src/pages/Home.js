22f0a498e57586587764620a98b8e6888b7d659e
felps <felipetec.sso@gmail.com>
2023-08-24 22:56:09 -0300

    feature: refatorando home, excluindo renderização 3D

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,59 +1,6 @@
-import React, { Suspense } from 'react';
-import { Canvas, useLoader, useFrame } from '@react-three/fiber';
-import { TextureLoader } from 'three';
+import React from 'react';
 import videoSource from '../assets.js/images/video.mp4'; // Importar o vídeo
-import codeTexture1 from '../assets.js/images/code1.jpg';
-import codeTexture2 from '../assets.js/images/code2.jpg';
-import codeTexture3 from '../assets.js/images/code3.jpg';
 import '../assets.js/styles/home.css';
-
-
-const RotatingBlock = ({
-  position,
-  texture,
-  rotationSpeed = 0.01,
-  scale = 1.5,
-}) => {
-  const loadedTexture = useLoader(TextureLoader, texture);
-  const ref = React.useRef();
-
-  // Adicionando rotação dentro da animação de frame
-  useFrame(() => {
-    if (ref.current) {
-      ref.current.rotation.x += rotationSpeed;
-      ref.current.rotation.y += rotationSpeed;
-    }
-  });
-
-  const handleMouseOver = (e) => {
-    e.object.scale.set(scale, scale, scale);
-    document.body.style.cursor = 'pointer'; // Alterar o cursor para "mãozinha"
-  };
-
-  const handleMouseOut = (e) => {
-    e.object.scale.set(1, 1, 1);
-    document.body.style.cursor = 'default'; // Voltar o cursor ao padrão
-  };
-
-  return (
-    <mesh
-      ref={ref}
-      position={position}
-      onPointerOver={handleMouseOver}
-      onPointerOut={handleMouseOut}
-    >
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial map={loadedTexture} />
-    </mesh>
-  );
-};
-
-const Loading = () => (
-  <mesh position={[0, 0, 0]}>
-    <boxGeometry args={[1, 1, 1]} />
-    <meshStandardMaterial color={'#888'} />
-  </mesh>
-);
 
 const Home = () => (
   <div className="home">
@@ -63,24 +10,13 @@ const Home = () => (
       </video>
     </div>
     <h1 className="title">
-      Bem vindo ao<span>CodeHub</span>
+      Bem vindo ao<span> CodeHub</span>
     </h1>
     <p>
       Você encontrará documentações completas e atualizadas sobre
       <br /> várias tecnologias front-end. Navegue por guias, tutoriais e
       referências <br /> abrangentes.
     </p>
-    <div className="canvas-container">
-      <Canvas>
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} />
-        <Suspense fallback={<Loading />}>
-          <RotatingBlock position={[-2, 0, 0]} texture={codeTexture1} />
-          <RotatingBlock position={[2, 0, 0]} texture={codeTexture2} />
-          <RotatingBlock position={[0, 0, 0]} texture={codeTexture3} />
-        </Suspense>
-      </Canvas>
-    </div>
     <div className="additional-section">
       <h2>Explore Mais Tecnologias</h2>
       <p>
