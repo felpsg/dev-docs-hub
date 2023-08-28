458ebd831c724ce1749967dd0a369b6ad9a8bd79
felps <felipetec.sso@gmail.com>
2023-08-28 14:29:17 -0300

    feature: add componente login/cadastro

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom'; // Adicione o 'useNavigate' aqui
 import '../assets.js/styles/header.css';
 import rocketIcon from '../assets.js/images/rocket-icon1.png';
-import { Link } from 'react-router-dom';
+import { auth } from '../firebaseConfig';
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from 'firebase/auth';
 
 const Header = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+  const navigate = useNavigate(); // useNavigate em vez de useHistory
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.log('Usuário logado com sucesso:', userCredential);
+        navigate('/'); // Redireciona para a página inicial
+      })
+      .catch((error) => {
+        console.error('Erro no login:', error);
+        console.error('Código do erro:', error.code);
+        console.error('Mensagem do erro:', error.message);
+      });
+  };
+
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.log('Usuário criado com sucesso:', userCredential);
+        navigate('/'); // Redireciona para a página inicial
+      })
+      .catch((error) => {
+        console.error('Erro no cadastro:', error);
+        console.error('Código do erro:', error.code);
+        console.error('Mensagem do erro:', error.message);
+      });
+  };
+
   return (
     <header>
       <nav className="navbar">
@@ -15,10 +53,20 @@ const Header = () => {
             </Link>
           </div>
           <div className="right-container">
-            <button type="button" className="btn btn-outline-success" data-bs-toggle="modal" data-bs-target="#loginModal">
+            <button
+              type="button"
+              className="btn btn-outline-success"
+              data-bs-toggle="modal"
+              data-bs-target="#loginModal"
+            >
               Login
             </button>
-            <button type="button" className="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#signupModal">
+            <button
+              type="button"
+              className="btn btn-outline-secondary"
+              data-bs-toggle="modal"
+              data-bs-target="#signupModal"
+            >
               Cadastro
             </button>
           </div>
@@ -31,19 +79,37 @@ const Header = () => {
           <div className="modal-content">
             <div className="modal-header">
               <h5 className="modal-title">Login</h5>
-              <button type="button" className="btn-close" data-bs-dismiss="modal"></button>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+              ></button>
             </div>
             <div className="modal-body">
-              <form>
+              <form onSubmit={handleLogin}>
                 <div className="mb-3">
                   <label className="form-label">Email</label>
-                  <input type="email" className="form-control" placeholder="Seu email" />
+                  <input
+                    type="email"
+                    className="form-control"
+                    placeholder="Seu email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
                 </div>
                 <div className="mb-3">
                   <label className="form-label">Senha</label>
-                  <input type="password" className="form-control" placeholder="Sua senha" />
+                  <input
+                    type="password"
+                    className="form-control"
+                    placeholder="Sua senha"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
                 </div>
-                <button type="submit" className="btn btn-outline-success">Entrar</button>
+                <button type="submit" className="btn btn-outline-success">
+                  Entrar
+                </button>
               </form>
             </div>
           </div>
@@ -56,23 +122,47 @@ const Header = () => {
           <div className="modal-content">
             <div className="modal-header">
               <h5 className="modal-title">Cadastro</h5>
-              <button type="button" className="btn-close" data-bs-dismiss="modal"></button>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+              ></button>
             </div>
             <div className="modal-body">
-              <form>
+              <form onSubmit={handleSignUp}>
                 <div className="mb-3">
                   <label className="form-label">Nome</label>
-                  <input type="text" className="form-control" placeholder="Seu nome" />
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Seu nome"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
                 </div>
                 <div className="mb-3">
                   <label className="form-label">Email</label>
-                  <input type="email" className="form-control" placeholder="Seu email" />
+                  <input
+                    type="email"
+                    className="form-control"
+                    placeholder="Seu email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
                 </div>
                 <div className="mb-3">
                   <label className="form-label">Senha</label>
-                  <input type="password" className="form-control" placeholder="Sua senha" />
+                  <input
+                    type="password"
+                    className="form-control"
+                    placeholder="Sua senha"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
                 </div>
-                <button type="submit" className="btn btn-outline-secondary">Cadastrar</button>
+                <button type="submit" className="btn btn-outline-secondary">
+                  Cadastrar
+                </button>
               </form>
             </div>
           </div>
